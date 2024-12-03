5237bd8d41b7c6efaf5651dd40e43988df6aab2e
Robert Burnett <dr.slate2@gmail.com>
2024-12-03 16:00:00 -0600

    finish day 3

--- 2024/3.lisp
+++ 2024/3.lisp
@@ -1,19 +1,28 @@
 
 (defparameter *data-file* "3.dat")
 
+(defun parse-integer-orelse-0 (string)
+  (handler-case
+      (parse-integer string)
+    (error (e)
+      (declare (ignore e))
+      0)))
+
 (defun attempt-match (string index)
   "Attempt to match the string starting at index start, returns nil or a number"
-  (ignore-errors
-   (when (= index (search "mul(" string :start2 index))
+   (when (equalp index (search "mul(" string :start2 index))
      (let*
          ((digit-index (+ 4 index))
           (close-paren-index (search ")" string :start2 digit-index))
           (digit-string-raw (subseq string digit-index close-paren-index))
           (substrs (uiop:split-string digit-string-raw :separator ","))
-          (digit-strings (subseq substrs 0 2))
-          (a (parse-integer (first digit-strings)))
-          (b (parse-integer (second digit-strings))))
-       (return-from attempt-match (* a b)))))
+          (digit-strings (progn
+                           (when (not (= (length substrs) 2))
+                             (return-from attempt-match 0))
+                           (subseq substrs 0 2)))
+          (a (parse-integer-orelse-0 (first digit-strings)))
+          (b (parse-integer-orelse-0 (second digit-strings))))
+       (return-from attempt-match (* a b))))
   0)
                   
 
@@ -25,5 +34,37 @@
     (loop
       :for i from 0
       :while (< i len)
-      :sum (attempt-match input i))))
+      :sum (let*
+               ((result (attempt-match input i)))
+             (when (not (= 0 result))
+               (format t "~a~%" (subseq input i
+                                        (search ")" input :start2 i))))
+             result))))
+
+(defun enable-mul?(string index)
+  (equalp index (search "do()" string :start2 index)))
+
+(defun disable-mul?(string index)
+  (equalp index (search "don't()" string :start2 index)))
+    
+;;; Part II
+(defun sum-multiplications-do-dont()
+  (let*
+      ((input (uiop:read-file-string *data-file*))
+       (len (length input))
+       (enabled t))
+    (loop
+      :for i from 0
+      :while (< i len)
+      :sum (progn
+             (when (enable-mul? input i)
+               (format t "~a~%" (subseq input i (+ i 4)))
+               (setf enabled t))
+             (when (disable-mul? input i)
+               (format t "~a~%" (subseq input i (+ i 7)))
+               (setf enabled nil))
+             (if enabled
+                 (attempt-match input i)
+                 0 )))))
+               
     
